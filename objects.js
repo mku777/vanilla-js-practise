ca1d40e2cd824669774a9b2b9556e4cf6c1c24de
mku777 <mkm.kuz777@gmail.com>
2023-06-29 13:11:44 +0300

    Update objects.js

--- objects.js
+++ objects.js
@@ -36,12 +36,12 @@
 
 // console.log (family);
 
-const family = [
-  { name: "Ann", online: true },
-  { name: "Max", online: false },
-  { name: "Jara", online: false },
-  { name: "Thorin", online: true },
-];
+// const family = [
+//   { name: "Ann", online: true },
+//   { name: "Max", online: false },
+//   { name: "Jara", online: false },
+//   { name: "Thorin", online: true },
+// ];
 
 // function getByName(allfriends, name) {
 //   for (const fam of family) {
@@ -75,19 +75,185 @@ const family = [
 
 // console.log(geOnlineFriends(family));
 
-function getFreindsByStatus(allFriends) {
-  const status = {
-    online: [],
-    offline: [],
-  };
-  for (const fam of allFriends) {
-    if (fam.online) status.online.push(fam.name);
-    else {
-      status.offline.push(fam.name);
+// function getFreindsByStatus(allFriends) {
+//   const status = {
+//     online: [],
+//     offline: [],
+//   };
+//   for (const fam of allFriends) {
+//     if (fam.online) status.online.push(fam);
+//     else {
+//       status.offline.push(fam);
+//     }
+//   }
+
+//   return status;
+// }
+
+// console.log(getFreindsByStatus(family));
+
+// const x = {
+//   a: 1,
+//   b: 2,
+//   c: 3,
+//   d: 4,
+//   e: 5,
+// }
+
+// const x1 = Object.keys(x).length;
+// console.log(x1);
+
+
+
+// Shopping Cart
+
+
+
+
+// const cart = {
+//   items: [],
+//   getItems() {
+//     return this.items;
+//   },
+//   add(product) {
+//     return this.items.push(product);
+//   },
+//   removeProduct(productName) {
+//     let newArray = [];
+//     for (const item of this.items) {
+//       newArray.push(item.name);
+//     }
+//     const toDelete = newArray.indexOf(productName);
+//     return this.items.splice(toDelete, 1);
+//   },
+//   clear() {
+//    return this.items = [];
+//   },
+//   countTotalPrice() {
+//     let totalPrice = 0;
+//     for (const product of this.items) {
+//       totalPrice += product.price;
+//     }
+//     return totalPrice;
+//   },
+//   increaseQuantity(productName) {
+//    this.items.quantity += 1;
+//   },
+//   descreaseQuantity(productName) { },
+  
+// };
+
+// cart.add({ name: "Warcraft", price: 10 });
+// cart.add({ name: "Starcraft", price: 20 });
+// cart.add({ name: 'Skyrim', price: 30 });
+// cart.add({ name: 'StarWars', price: 40 });
+
+// // console.table(cart.removeProduct("StarWars"));
+
+// // console.table(cart.clear());
+// // console.log(cart.countTotalPrice());
+// console.log(cart.increaseQuantity('Warcraft'));
+// console.table(cart.getItems());
+
+
+
+
+// spread, destr
+
+
+// const temperature = [20, 21, 15, 14, 10, 15, 30]
+
+// const lastWeek = [1, 2, 3]
+// const current = [4, 5, 6];
+// const next = [7, 8, 9];
+
+// let allTemps = [];
+
+
+// console.log(Math.min(...temperature, ...lastWeek));
+
+// const goToWalk = {
+//     where: 'Ternopil',
+//     time: '20.20',
+//     adress: 'NovSvit',
+// }
+
+// const juleWalk = {
+//   where: "Kharkiv",
+//   adress: "Biblik",
+// };
+
+
+// const newWalk = {
+//     ...goToWalk,
+//     ...juleWalk
+// }
+// console.log(newWalk);
+
+
+
+//    const playlist = {
+//      artist: "Progigy",
+//      album: "The Fat of the Land",
+//      tracks: ["track1", "track2", "track3", "track4", "track5", "track6"],
+//      trackCount: 6,
+//      rating: 10,
+//      year: "1997",
+//      genre: "rave",
+//      stats: {
+//        num1: "1",
+//        num2: "2",
+//        num3: "3",
+//      },
+//      changeAlbum(newAlbum) {
+//        this.album = newAlbum;
+//      },
+//      addTrack(track) {
+//        this.tracks.push(track);
+//      },
+//      updateCount(count) {
+//        this.trackCount = count;
+//      },
+//    };
+
+// const { tracks, album: al, genre, year, stats: { num1, num2, num3 } } = playlist;
+// playlist.stats.num4 = 'exp';
+
+// console.log(playlist);
+
+
+
+// console.log(playlist["genre"]);
+
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  // Change code below this line
+  let valuesArray = [];
+
+  for (const product of products) {
+    if (product[propName]) {
+      valuesArray.push(product[propName]);
     }
   }
 
-  return status;
+  return valuesArray;
+
+  // Change code above this line
 }
 
-console.log(getFreindsByStatus(family));
+
+
+
+
+
+console.log(getAllPropValues("name"));
+console.log(getAllPropValues("quantity"));
+console.log(getAllPropValues("price"));
+console.log(getAllPropValues("category"));
