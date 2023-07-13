bda7a57d1f017b71da1a1a14b115c22c900a1bdd
mku777 <mkm.kuz777@gmail.com>
2023-07-13 20:23:19 +0300

    Update objects.js

--- objects.js
+++ objects.js
@@ -343,3 +343,21 @@
 // document.body.insertAdjacentHTML("afterbegin", playlistInfo(playlist));
 
 
+
+function findMatches(arr, ...args) {
+  const matches = [];
+
+  for  (const num of args) {
+    if (arr.includes(num)) {
+      matches.push(num);
+    }
+  }
+  return matches;
+
+}
+
+
+console.log(findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7));
+console.log(findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2))
+console.log(findMatches([10, 24, 41, 6, 9, 19], 24, 11, 9, 23, 41))
+
