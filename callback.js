4af2201856aa5aee4120bf24c37829841a735b31
mku777 <mkm.kuz777@gmail.com>
2023-07-17 12:15:13 +0300

    Update callback.js

--- callback.js
+++ callback.js
@@ -33,33 +33,48 @@
 
 
 
-function filter(arr, test) {
-  const filteredArr = [];
-  for (const num of arr) {
-      if (test(num)) {
-        filteredArr.push(num);
-      }
-  }
-  return filteredArr;
-};
-
-function biggerNum(num) {
-  return num > 3;
-}
-
-console.log(filter([1, 2, 3, 4, 5, 6, 7], biggerNum));
-
-
-
-
-
-
-
 // const isAlbum = [
 //   { name: "Pluto", year: 2023, status: "got" },
 //   { name: "Mars", year: 1998, status: "on the way" },
 //   { name: "Moon", year: 2003, status: "have" },
 //   { name: "Uran", year: 2015, status: "have" },
 // ];
+
+// function filter(arr, test) {
+//   const filteredArr = [];
+//   for (const num of arr) {
+//       if (test(num)) {
+//         filteredArr.push(num);
+//       }
+//   }
+//   return filteredArr;
+// };
+
+// function biggerNum(arr) {
+//   if (arr.status === "got") {
+//     return arr.name;
+//   }
+// }
+
+// console.log(filter(isAlbum, biggerNum));
+
+
+const makeDish = function (name, dish) {
+  console.log(`${name} готовит ${dish}`);
+}
+
+
+
+makeDish('Max', 'borsch')
+makeDish("Ann", "meat");
+makeDish("Jara", "cherry");
+makeDish("Thorin", "chiken");
+
+
+
+
+
+
+
 
 
