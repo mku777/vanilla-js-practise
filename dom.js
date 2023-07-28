2802b39a527c8cfb394d754d6895c761192c865d
mku777 <mkm.kuz777@gmail.com>
2023-07-28 16:20:49 +0300

    upd

---
+++ dom.js
@@ -0,0 +1,14 @@
+
+
+// addEventListener('click', (evt) => {
+//     const navRef = document.querySelector("ul");
+//     console.log(navRef);
+
+//     const navLinksRef = navRef.querySelector("li");
+//     console.log(navLinksRef);
+// })
+
+
+const imageEl = document.querySelector(".hero-image");
+console.log(imageEl);
+console.log(imageEl.width = 550);
