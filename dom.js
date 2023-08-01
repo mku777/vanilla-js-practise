cc6e9e5a9043f622fca96160b6f24814e7fe0368
mku777 <mkm.kuz777@gmail.com>
2023-08-01 23:48:20 +0300

    dom

--- dom.js
+++ dom.js
@@ -1,5 +1,3 @@
-
-
 // addEventListener('click', (evt) => {
 //     const navRef = document.querySelector("ul");
 //     console.log(navRef);
@@ -12,8 +10,6 @@
 // mainButton.classList.add('btn-primary');
 // mainButton.textContent = "super button";
 // console.log(mainButton);
-
-
 
 // const imageEl = document.querySelector(".hero-image");
 
@@ -28,9 +24,7 @@
 
 // navAllEl.appendChild(newListEl);
 
-
 // navAllEl.insertBefore(mainButton, navAllEl.firstChild);
-
 
 // const colorPicker = [
 //   {
@@ -78,7 +72,6 @@
 //   elements.push(buttonPicker);
 // }
 
-
 // function makeColorPickerOptions(options) {
 //     return options.map((picker) => {
 //       const buttonPicker = document.createElement("button");
@@ -92,32 +85,116 @@
 
 // const elements = makeColorPickerOptions(colorPicker);
 
-
-
 // console.log(elements);
 
 // pickerWrap.append(...elements);
 
+// const playlist = {
+//   album: "The Fat of the Land",
+//   tracks: ["track1", "track2", "track3", "track4", "track5", "track6"],
+//   trackCount: 6,
+//   rating: 10,
+//   year: "1997",
+//   genre: "rave",
+// };
+
+// const artistEl = document.querySelector(".artistEl");
+// const albumEl = document.createElement("p");
+// const tracksEl = document.createElement("p");
+// const countEl = document.createElement("p");
+
+// albumEl.textContent = playlist.album;
+// tracksEl.textContent = playlist.tracks;
+// countEl.textContent = playlist.trackCount;
+
+// artistEl.append(albumEl, tracksEl, countEl);
+
+// console.log(artistEl);
 
 
-const playlist = {
-  album: "The Fat of the Land",
-  tracks: ["track1", "track2", "track3", "track4", "track5", "track6"],
-  trackCount: 6,
-  rating: 10,
-  year: "1997",
-  genre: "rave",
-};
+// const categoriesEL = document.querySelectorAll(".item");
+// console.log(`Number of categories: ${categoriesEL.length}`);
 
-const artistEl = document.querySelector(".artistEl");
-const albumEl = document.createElement("p");
-const tracksEl = document.createElement("p");
-const countEl = document.createElement("p");
 
-albumEl.textContent = playlist.album;
-tracksEl.textContent = playlist.tracks;
-countEl.textContent = playlist.trackCount;
+// categoriesEL.forEach(item => {
+//   const titleEl = item.querySelector("h2");
+//   console.log(`Category: ${titleEl.textContent}`);
+//   const liEl = item.querySelectorAll("li");
+//   console.log(`Elements : ${liEl.length}`);
+// });
 
-artistEl.append(albumEl, tracksEl, countEl);
+// const ingredients = [
+//   "Potatoes",
+//   "Mushrooms",
+//   "Garlic",
+//   "Tomatos",
+//   "Herbs",
+//   "Condiments",
+// ];
 
-console.log(artistEl);
+
+// const ulEl = document.querySelector("#ingredients");
+
+
+// ingredients.map((item) => {
+//   const itemElements = document.createElement("li");
+//   itemElements.className = 'item';
+//   itemElements.textContent = item;
+//   console.log(itemElements)
+//   ulEl.append(itemElements);
+// });
+
+
+// const images = [
+//   {
+//     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+//     alt: "White and Black Long Fur Cat",
+//   },
+//   {
+//     url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+//     alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+//   },
+//   {
+//     url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+//     alt: "Group of Horses Running",
+//   },
+// ];
+
+// const ulEl = document.querySelector(".gallery");
+// console.log(ulEl);
+
+// const gallery = images.map(({ url, alt }) => {
+//   return `<li><img src=${url} alt=${alt} width="250"></img></li>`;
+// });
+
+// ulEl.insertAdjacentHTML("beforeend", gallery);
+
+// ulEl.style.flexDirection = "column";
+
+
+// const incrementButton = document.querySelector("[data-action=increment");
+// const decrementButton = document.querySelector("[data-action=decrement");
+// const valueCounter = document.querySelector("#value");
+
+// let value = 0;
+
+// incrementButton.addEventListener("click", () => {
+//   value += 1;
+//   valueCounter.textContent = value;
+// });
+
+// decrementButton.addEventListener("click", () => {
+//   value -= 1;
+//   valueCounter.textContent = value;
+// });
+
+
+const inputEl = document.querySelector("#name-input");
+const spanEl = document.querySelector("#name-output");
+
+inputEl.addEventListener("input", (e) => {
+  spanEl.textContent = e.currentTarget.value;
+  if (e.currentTarget.value === "") {
+    spanEl.textContent = 'Anonymous';
+  }
+})
